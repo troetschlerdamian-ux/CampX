4114b602be214560c35c1f748841012e2758a192
troetschlerdamian-ux <troetschler.damian@gmail.com>
2026-01-28 13:38:29 +0100

    Delete campx-booking/campx-booking directory

--- campx-booking/campx-booking/templates/calendar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$capacity = (int) get_post_meta($rid,'_campx_capacity',true);
-?>
-<div class="campx-card">
-  <h3><?php echo sprintf(__('Kalender – %s','campx'), esc_html(get_the_title($rid)));?></h3>
-  <div class="campx-calendar" data-campx-calendar data-res-id="<?php echo esc_attr($rid);?>" data-capacity="<?php echo esc_attr($capacity);?>"></div>
-</div>
